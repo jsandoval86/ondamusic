06d3489203346f3c484751fd9da26bc35b21cdeb
Juan <juancsandoval86@gmail.com>
2016-10-25 20:53:02 -0500

    creacion de modelos Song, PlayList

--- app/models.py
+++ app/models.py
@@ -1,5 +1,31 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Song(models.Model):
+
+	name = models.CharField(max_length=50, null=False, blank=False)
+	artist = models.CharField(max_length=50, null=True,blank=True)
+
+	class Meta:
+		verbose_name = "Song"
+		verbose_name_plural = "Songs"
+		ordering = ['-name']
+
+	def __str__(self):
+		return u'%s' % (self.name)
+
+
+class PlayList(models.Model):
+
+	user = models.ForeignKey(User, related_name='playlist')
+	name = models.CharField(max_length=50, null=False, blank=False)
+	songs = models.ManyToManyField(Song)
+
+	class Meta:
+		verbose_name = "Play list"
+		verbose_name_plural = "Play lists"
+
+	def __str__(self):
+			return u'%s' % (self.name)
